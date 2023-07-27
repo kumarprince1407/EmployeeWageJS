1912d2f40d447d9c37d254c36da91a5074b97e22
Prince Kumar <kumarprince1407@gmail.com>
2023-07-27 16:15:19 +0530

    Added Use Case 4

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,60 +5,36 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-//UC1
-// let emp_check = Math.floor(Math.random() * 10) % 2;
-// if (emp_check == is_absent) {
-//   console.log("Employee is absent.");
-//   return;
-// } else {
-//   console.log("Employee is present");
-// }
-//UC2
-// const IS_PART_TIME = 1;
-// const IS_FULL_TIME = 2;
-// const PART_TIME_HOURS = 4;
-// const FULL_TIME_HOURS = 8;
-// const WAGE_PER_HOUR = 20;
-
-// let empHrs = 0;
-// let empCheck = Math.floor(Math.random() * 10) % 3;
-// switch (empCheck) {
-//   case IS_PART_TIME:
-//     empHrs = PART_TIME_HOURS;
-//     console.log("Employee is present part-time. ");
-//     break;
-
-//   case IS_FULL_TIME:
-//     empHrs = FULL_TIME_HOURS;
-//     console.log("Employee is present part-time. ");
-//     break;
-
-//   default:
-//     empHrs = 0;
-//     console.log("Employee is absent.");
-// }
-// let empWage = empHrs * WAGE_PER_HOUR;
-// console.log("Employee wage:" + empWage);
-
 //UC3
-let empHrs = 0;
+//let empHrs = 0;
 let empCheck = Math.floor(Math.random() * 10) % 3;
 function getWorkingHours(empCheck) {
   switch (empCheck) {
     case IS_PART_TIME:
-      console.log("Employee is present part-time. ");
+      // console.log("Employee is present part-time. ");
       return PART_TIME_HOURS;
 
     case IS_FULL_TIME:
-      console.log("Employee is present full-time. ");
+      //console.log("Employee is present full-time. ");
       return FULL_TIME_HOURS;
 
     default:
-      console.log("Employee is absent.");
+      //console.log("Employee is absent.");
       return 0;
   }
 }
 
-empHrs = getWorkingHours(empCheck);
+// empHrs = getWorkingHours(empCheck);
+// let empWage = empHrs * WAGE_PER_HOUR;
+// console.log("Employee wage:" + empWage);
+
+//UC 4
+//Calculating Wages for a Month assuming 20 Working Days in a Month
+const MONTLY_WORKING_DAYS = 20;
+let empHrs = 0;
+for (let day = 0; day < MONTLY_WORKING_DAYS; day++) {
+  let empCheck = Math.floor(Math.random() * 10) % 3;
+  empHrs += getWorkingHours(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee wage:" + empWage);
+console.log("Total working hours: " + empHrs + " | Employee wage: " + empWage);
